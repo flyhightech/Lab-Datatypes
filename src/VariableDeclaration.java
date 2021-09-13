6ee72e408781c4c659975783fe7856e8355893ac
Bernard Huff <flyhightech3@gmail.com>
2021-09-12 21:48:13 -0500

    Initial commit

---
+++ src/VariableDeclaration.java
@@ -0,0 +1,28 @@
+
+public class VariableDeclaration {
+	
+    public static void main(String[] args){
+    	
+    	// Below is a boolean value 
+        boolean on = false;
+        System.out.println(on);
+        
+        // Below is a short number
+        short s = 16; 
+        System.out.println(s);
+        
+        int i = -32; 
+        System.out.println(i);
+        
+        float f = 3839.34839f;
+        System.out.println(f);
+        
+        long l = 4294967296l; 
+        System.out.println(l);
+        
+        char c = 'a';
+        System.out.println(c);
+        
+    }
+
+}
